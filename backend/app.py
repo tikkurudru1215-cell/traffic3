b53e70d63dca5089292201644cf99ca115658456
tikkurudru1215-cell <tikkurudru1215@gmail.com>
2026-04-12 18:44:47 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -130,5 +130,7 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    # debug=True must be False on Render
+    app.run(host='0.0.0.0', port=port, debug=False)
